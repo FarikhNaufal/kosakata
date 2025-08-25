a01ece3c9dd13c9c0c4f984e83afad0ed783d73e
FarikhNaufal <mfarikh9@gmail.com>
2025-08-25 08:12:28 +0700

    update

--- internal/game/sambungkata/dto.go
+++ internal/game/sambungkata/dto.go
@@ -9,4 +9,5 @@ type WrongWordDTO struct {
 	Clue     []string `json:"clue"`
 	Length   int      `json:"length"`
 	PrevWord *string  `json:"prev_word"`
+	Position []int    `json:"position"`
 }
--- internal/game/sambungkata/handler.go
+++ internal/game/sambungkata/handler.go
@@ -53,6 +53,92 @@ func (h *WordHandler) GetTodayWord(ctx *gin.Context) {
 	response.Success(ctx, "Success", res)
 }
 
+type CheckWord struct {
+	Success  bool
+	Clue     string
+	Progress float64
+	Position []int
+}
+
+// type WordPosition struct {
+// 	Position []int
+// 	Color string
+// }
+
+func CheckWordPosition(word, answer string) []int {
+	maxLen := len(word)
+	if len(answer) > maxLen {
+		maxLen = len(answer)
+	}
+
+	result := make([]int, maxLen)
+	freq := make(map[rune]int)
+
+	for _, ch := range answer {
+		freq[ch]++
+	}
+
+	// step 1: posisi sama
+	for i := 0; i < maxLen; i++ {
+		if i < len(word) && i < len(answer) && word[i] == answer[i] {
+			result[i] = 1
+			freq[rune(word[i])]--
+		}
+	}
+
+	// step 2: posisi beda
+	for i := 0; i < len(word); i++ {
+		ch := rune(word[i])
+		if result[i] == 0 && freq[ch] > 0 {
+			result[i] = 2
+			freq[ch]--
+		}
+	}
+
+	return result
+}
+
+func MatchingWord(list []string, nextWordRequest NextWordRequest, word Word) (res CheckWord, error error) {
+	res.Success = false
+	res.Progress = 0.0
+
+	if nextWordRequest.PrevWord == nil {
+		res.Position = CheckWordPosition(nextWordRequest.NextWord, list[0])
+		if strings.EqualFold(nextWordRequest.NextWord, list[0]) {
+			res.Success = true
+			res.Progress = 1 / float64(len(list)+1) * 100
+		}
+	} else {
+		for i := range list {
+			// res.Position = CheckWordPosition(nextWordRequest.NextWord, list[i+1])
+			res.Clue = list[i]
+			if i < len(list)-1 {
+				if strings.EqualFold(*nextWordRequest.PrevWord, list[i]) &&
+					strings.EqualFold(nextWordRequest.NextWord, list[i+1]) {
+					res.Success = true
+					res.Progress = float64(i+2) / float64(len(list)+1) * 100
+
+					break
+				}
+			}
+
+			if len(list)-1 == i {
+				// res.Position = CheckWordPosition(nextWordRequest.NextWord, word.End)
+				res.Clue = word.End
+				if strings.EqualFold(nextWordRequest.NextWord, word.End) {
+					res.Success = true
+					res.Progress = float64(i+2) / float64(len(list)+1) * 100
+					break
+				}
+			}
+
+		}
+	}
+
+	res.Progress = math.Round(res.Progress*100) / 100
+	return
+}
+
 func (h *WordHandler) CheckingNextWord(ctx *gin.Context) {
 	var nextWordRequest NextWordRequest
 	err := ctx.ShouldBindBodyWithJSON(&nextWordRequest)
@@ -75,48 +161,22 @@ func (h *WordHandler) CheckingNextWord(ctx *gin.Context) {
 
 	var list []string
 	if err := json.Unmarshal(word.List, &list); err != nil {
+		response.Failed(ctx, http.StatusInternalServerError, nil, err)
 		return
 	}
 
-	success := false
-	prev_word := nextWordRequest.PrevWord
-	var clue string
-	progress := 0.0
-
-	if nextWordRequest.PrevWord == nil {
-		if strings.EqualFold(nextWordRequest.NextWord, list[0]) {
-			success = true
-			progress = 1 / float64(len(list)+1) * 100
-		}
-	} else {
-		for i := range list {
-			if i < len(list)-1 &&
-				strings.EqualFold(*nextWordRequest.PrevWord, list[i]) &&
-				strings.EqualFold(nextWordRequest.NextWord, list[i+1]) {
-				success = true
-				progress = float64(i+2) / float64(len(list)+1) * 100
-
-				clue = list[i+1]
-				break
-			}
-
-			if len(list)-1 == i {
-				if strings.EqualFold(nextWordRequest.NextWord, word.End) {
-					success = true
-					progress = float64(i+2) / float64(len(list)+1) * 100
-					break
-				}
-				clue = word.End
-			}
-
-		}
+	matcher, err := MatchingWord(list, nextWordRequest, word)
+	if err != nil {
+		response.Failed(ctx, http.StatusInternalServerError, nil, err)
 	}
 
-	if !success {
+	if !matcher.Success {
 		res := WrongWordDTO{
-			Clue:     []string{string(clue[0]), string(clue[len(clue)-1])},
-			Length:   len(clue),
-			PrevWord: prev_word,
+			// Clue:     []string{string(matcher.Clue[0])},
+			Clue:     []string{matcher.Clue},
+			Length:   len(matcher.Clue),
+			PrevWord: nextWordRequest.PrevWord,
+			Position: matcher.Position,
 		}
 
 		msg := "Wrong next word."
@@ -126,7 +186,7 @@ func (h *WordHandler) CheckingNextWord(ctx *gin.Context) {
 	response.Success(ctx, "Success",
 		gin.H{
 			"prev_word": nextWordRequest.NextWord,
-			"progress":  math.Round(progress*100) / 100,
+			"progress":  matcher.Progress,
 		},
 	)
 
